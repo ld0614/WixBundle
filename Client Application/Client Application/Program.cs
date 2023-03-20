748417ddc92e07c17a2cfe7fc99d0b1693cd9c71
Leo D'Arcy <leo.darcy@poweronplatforms.com>
2023-03-20 22:43:56 +0000

    Initial Commit

---
+++ Client Application/Client Application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Client_Application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm a console Application");
+        }
+    }
+}
---
+++ Client Application/ClientApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm also a console Application");
+        }
+    }
+}
